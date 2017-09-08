f53565ebea11c955f14da89ed31feefa3b608f0d
sobhy97 <sobhy6452@gmail.com>
2017-09-09 00:57:19 +0200

    This is a Test

---
+++ js/plug.js
@@ -0,0 +1,33 @@
+// $(function () {
+//     $('#myCarousel').carousel({
+//         interval:2000,
+//         pause: "false"
+//     });
+//     $('#playButton').click(function () {
+//         $('#myCarousel').carousel('cycle');
+//     });
+//     $('#pauseButton').click(function () {
+//         $('#myCarousel').carousel('pause');
+//     });
+// });
+$('#myCarousel').hover(function () { 
+  $(this).carousel('pause') 
+}, function () { 
+  $(this).carousel('cycle') 
+});
+$('#myCarousel').hover(function () { 
+  $(this).carousel('pause') 
+}, function () { 
+  $(this).carousel('cycle') 
+})
+
+$('#blogSlider').hover(function () { 
+  $(this).carousel('pause') 
+}, function () { 
+  $(this).carousel('cycle') 
+});
+$('#blogSlider').hover(function () { 
+  $(this).carousel('pause') 
+}, function () { 
+  $(this).carousel('cycle') 
+})
